a84e6523efb94ca3456c820f121ac54b0e2b0530
Mobile Developer <mobiledeveloper@Mobiles-Mac.local>
2021-05-10 13:07:38 -0700

    finished

---
+++ models/bitcoinUnits.js
@@ -0,0 +1,12 @@
+export const BitcoinUnit = Object.freeze({
+  BTC: 'BTC',
+  SATS: 'sats',
+  LOCAL_CURRENCY: 'local_currency',
+  MAX: 'MAX',
+  XEP: 'XEP',
+});
+
+export const Chain = Object.freeze({
+  ONCHAIN: 'ONCHAIN',
+  OFFCHAIN: 'OFFCHAIN',
+});
